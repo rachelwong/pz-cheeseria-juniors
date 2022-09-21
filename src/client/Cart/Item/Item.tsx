0b8e4884d93e3d73d9431ea25f61bf001ce08578
rachel-wong <hello.rachelwong@gmail.com>
2022-09-21 18:27:16 +1000

    wip: added open dialog box on items

--- src/client/Cart/Item/Item.tsx
+++ src/client/Cart/Item/Item.tsx
@@ -3,23 +3,81 @@ import Button from '@material-ui/core/Button';
 import { CartItemType } from '../../App';
 // Styles
 import { Wrapper } from './Item.styles';
+import {DialogTitle, Dialog, DialogContent, DialogActions } from '@material-ui/core';
 
-type Props = {
-  item: CartItemType;
-  handleAddToCart: (clickedItem: CartItemType) => void;
-};
+// type Props = {
+//   item: CartItemType;
+//   handleAddToCart: (clickedItem: CartItemType) => void;
+// };
 
-const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
-  <Wrapper>
+// const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
+
+//   <Wrapper>
+//     <img src={item.image} alt={item.title} />
+//     <div>
+//       <h3>{item.title}</h3>
+//       <h3>${item.price}</h3>
+//     </div>
+//     <Dialog
+//         open={open}
+//         onClose={() => setOpen(false)}
+//         aria-labelledby="confirm-dialog"
+//       >
+//         <DialogTitle id="confirm-dialog">{item.title}</DialogTitle>
+//         <DialogContent>{item.description}</DialogContent>
+//         <DialogActions>
+//           <Button
+//             variant="contained"
+//             onClick={() => setOpen(false)}
+//             color="secondary"
+//           >
+//             Close
+//           </Button>
+//         </DialogActions>
+//       </Dialog>
+//     <Button
+//       onClick={() => handleAddToCart(item)}
+//       data-cy={`add-to-cart-${item.id}`}>Add to cart</Button>
+//   </Wrapper>
+// );
+
+// export default Item;
+
+import React, { useState } from 'react'
+
+const Item = ({ item, handleAddToCart }) => {
+
+  const [open, setOpen] = useState(false)
+
+  return (
+    <Wrapper onClick={ () => setOpen(true)}>
     <img src={item.image} alt={item.title} />
     <div>
       <h3>{item.title}</h3>
       <h3>${item.price}</h3>
     </div>
+    <Dialog
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="confirm-dialog"
+      >
+        <DialogTitle id="confirm-dialog">{item.title}</DialogTitle>
+        <DialogContent>{item.description}</DialogContent>
+        <DialogActions>
+          <Button
+            variant="contained"
+            onClick={() => console.log("Close")}
+            color="secondary"
+          >
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
     <Button
       onClick={() => handleAddToCart(item)}
       data-cy={`add-to-cart-${item.id}`}>Add to cart</Button>
   </Wrapper>
-);
+  )
+}
 
-export default Item;
+export default Item
