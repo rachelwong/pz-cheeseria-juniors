85d7ff06685f28de8d575280c51c736a0a1b24ae
rachel-wong <hello.rachelwong@gmail.com>
2022-09-21 18:53:45 +1000

    fix: revert item back to typescript functional component

--- src/client/Cart/Item/Item.tsx
+++ src/client/Cart/Item/Item.tsx
@@ -4,83 +4,48 @@ import { CartItemType } from '../../App';
 // Styles
 import { Wrapper } from './Item.styles';
 import {DialogTitle, Dialog, DialogContent, DialogActions } from '@material-ui/core';
+import { useState } from 'react'
 
-// type Props = {
-//   item: CartItemType;
-//   handleAddToCart: (clickedItem: CartItemType) => void;
-// };
+type Props = {
+  item: CartItemType;
+  handleAddToCart: (clickedItem: CartItemType) => void;
+};
 
-// const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
-
-//   <Wrapper>
-//     <img src={item.image} alt={item.title} />
-//     <div>
-//       <h3>{item.title}</h3>
-//       <h3>${item.price}</h3>
-//     </div>
-//     <Dialog
-//         open={open}
-//         onClose={() => setOpen(false)}
-//         aria-labelledby="confirm-dialog"
-//       >
-//         <DialogTitle id="confirm-dialog">{item.title}</DialogTitle>
-//         <DialogContent>{item.description}</DialogContent>
-//         <DialogActions>
-//           <Button
-//             variant="contained"
-//             onClick={() => setOpen(false)}
-//             color="secondary"
-//           >
-//             Close
-//           </Button>
-//         </DialogActions>
-//       </Dialog>
-//     <Button
-//       onClick={() => handleAddToCart(item)}
-//       data-cy={`add-to-cart-${item.id}`}>Add to cart</Button>
-//   </Wrapper>
-// );
-
-// export default Item;
-
-import React, { useState } from 'react'
-
-const Item = ({ item, handleAddToCart }) => {
-
-  const [open, setOpen] = useState(false)
-  console.log('open:', open)
+const Item: React.FC<Props> = ({ item, handleAddToCart }) => {
+  const [open, setOpen] = useState(false);
 
   return (
     <>
-    <Wrapper  >
-    <img src={item.image} alt={item.title} onClick={ () => setOpen(true)}/>
-    <div>
-      <h3>{item.title}</h3>
-      <h3>${item.price}</h3>
-    </div>
+    <Wrapper>
+        <img src={item.image} alt={item.title} onClick={ () => setOpen(true)} />
+      <div>
+        <h3>{item.title}</h3>
+        <h3>${item.price}</h3>
+      </div>
     <Button
       onClick={() => handleAddToCart(item)}
       data-cy={`add-to-cart-${item.id}`}>Add to cart</Button>
       </Wrapper>
     <Dialog
-        open={open}
-        onClose={() => setOpen(false)}
-        aria-labelledby="confirm-dialog"
-      >
-        <DialogTitle id="confirm-dialog">{item.title}</DialogTitle>
-        <DialogContent>{item.description}</DialogContent>
-        <DialogActions>
-          <Button
-            variant="contained"
-            onClick={() => setOpen(false)}
-            color="secondary"
-          >
-            Close
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </>
-  )
-}
+         open={open}
+         onClose={() => setOpen(false)}
+         aria-labelledby="confirm-dialog"
+       >
+         <DialogTitle id="confirm-dialog">{item.title}</DialogTitle>
+         <DialogContent>{item.description}</DialogContent>
+         <DialogActions>
+           <Button
+             variant="contained"
+             onClick={() => setOpen(false)}
+             color="secondary"
+           >
+             Close
+           </Button>
+         </DialogActions>
+       </Dialog>
 
-export default Item
+    </>
+  );
+};
+
+export default Item;
